b2f36a910ddd52e952aa9fc42187cc211f6b44f9
EddyScotch <40645214+EddyScotch@users.noreply.github.com>
2018-07-31 12:37:50 -0700

    Add files via upload

---
+++ Blink/Blink.ino
@@ -0,0 +1,46 @@
+int led = 13;
+int time_length = 500;
+
+void setup() {
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+p();
+i();
+g();
+}
+
+void dot (){
+  digitalWrite(led, HIGH);
+  delay(time_length);
+  digitalWrite(led, LOW);
+  delay(time_length);   
+}
+
+void dash (){
+  digitalWrite(led, HIGH);
+  delay(time_length*2);
+  digitalWrite(led, LOW);
+  delay(time_length*3);
+}
+
+void p (){
+  dot ();
+  dash();
+  dash();
+  dot ();
+}
+
+void i (){
+  dot();
+  dot();
+}
+
+void g () {
+  dash();
+  dash();
+  dot();
+}
+
+
---
+++ Whisker/Whisker.ino
@@ -0,0 +1,27 @@
+int leftWhisker = 5;
+int rightWhisker = 7;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin (9600);
+  pinMode (leftWhisker, INPUT);
+  pinMode (rightWhisker, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int left_value = digitalRead (leftWhisker);
+  int right_value = digitalRead (rightWhisker);
+  
+  if (right_value == 0 && left_value == 0){
+    Serial.println ("both touched");
+  }
+  else if (left_value == 0) {
+    Serial.println ("only leftWhisker touched");
+  }
+  else if (right_value == 0) {
+    Serial.println ("only rightWhisker touched");
+  }
+ 
+  delay(500);
+}
